7529b78289dca6ad7d75d8f47c6e00aaac84de9c
Chris Van Arsdale <chris.vana@gmail.com>
2013-10-20 23:02:44 -0700

    parser

--- Parser.java
+++ Parser.java
@@ -1,15 +1,22 @@
 // Example library
 
+import com.google.protobuf.TextFormat;
+import com.google.protobuf.TextFormat.ParseException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.PrintStream;
 
 public class Parser {
   public void Parse(InputStream input, OutputStream output) throws IOException {
-    // For now, do nothing but mirror the input.
-    int character ;
-    while ((character = input.read()) >= 0) {
-      output.write(character);
+    MyProto.FooProto.Builder builder = MyProto.FooProto.newBuilder();
+    PrintStream stream = new PrintStream(output);
+    try {
+      TextFormat.merge(new InputStreamReader(input, "ASCII"), builder);
+      stream.println("Success!");
+    } catch (ParseException e) {
+      stream.println("Failed to Parse ASCII input: " + e);
     }
   }
 }
